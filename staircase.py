20963a7f341e8082fc83a151f909dfd10307ff1b
kiranajij <kiranajij216@gmail.com>
2019-04-20 20:58:30 +0530

    added staircase algorithm

---
+++ staircase.py
@@ -0,0 +1,50 @@
+def staircase(height: int, step_size: list):
+    if height < 0:
+        return 0
+    ways = 0
+    for i in step_size:
+        if height == i:
+            ways += 1
+        else:
+            ways += staircase(height - i, step_size)
+    return ways
+    # ways = 1
+    # for i in step_size:
+    #     ways += staircase(height-i, step_size)
+    # return ways
+
+
+def implementation_2(height: int, step_sizes: list) -> int:
+    """
+    :raises: No exception.
+    :rtype: integer denoting number of ways.
+    :param height: height of the staircase
+    :param step_sizes: Allowed step sizes, step_sizes is required to be sorted.
+    :return: return the number of ways to reach to the top
+            with the available step sizes.
+    :example:
+        Q. How many ways there are to reach to the top of a staircase of height
+            5 with allowed step sizes {1, 2}
+        Ans.
+            >>> implementation_2(5, [1, 2])
+            >>> 8
+        thus number of ways is 8.
+
+
+    """
+    ways = 0
+    if height == 0:
+        return 1
+    elif height == step_sizes[0]:
+        return 1
+    else:
+        for size in step_sizes:
+            if height >= size:
+                ways += implementation_2(height-size, step_sizes)
+        return ways
+
+
+if __name__ == '__main__':
+    param = [4, [1, 2]]
+    # print(staircase(*param))
+    print(implementation_2(*param))
