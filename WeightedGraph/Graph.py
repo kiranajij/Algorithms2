dc11563ff95c25d2fa98854e8f5dcd83dce53e89
kiranajij <kiranajij216@gmail.com>
2019-04-24 20:25:44 +0530

    polished weighted graph class

--- WeightedGraph/Graph.py
+++ WeightedGraph/Graph.py
@@ -1,8 +1,25 @@
 class WeightedGraph:
+    """
+    This is a data structure to hold a graph.
+    """
     def __init__(self, graph: dict):
+        """
+        :param graph: graph is of type dict. The graph must be in format
+                {
+                    vertex: [(edge_vertex, weight)]
+                }
+                where vertex is a vertex of the graph
+                edge_vertex is a edge. for a graph G = (V, E)
+                if (u, v) belongs to E and weight of (u, v) is w then
+                (v, w) belongs to graph[u].
+        """
         self.graph = graph
 
     def get_neighbors(self, v):
+        """
+        :param v: the vertex whose neighbors are desired
+        :return: returns the neighbors of v
+        """
         neighbors = [i[0] for i in self.graph[v]]
         return neighbors
 
@@ -16,9 +33,21 @@ class WeightedGraph:
         weights = [i[1] for i in self.graph[v]]
         return weights
 
+    def get_min_weighted_pair(self, v):
+        """
+        This function returns the minimum weighted pair from the neighbor of v
+        :param v: vertex
+        :return: tuple
+        """
+        tmp: list = self.graph[v]
+        if not tmp:         # if
+            return None
+        min_pair = min(tmp, key=lambda x: x[1])     # getting the minimum
+        return min_pair
+
     def __str__(self) -> str:
-        return super().__str__()
+        return self.graph.__str__()
 
     def __repr__(self) -> str:
-        return super().__repr__()
+        return "WeightedGraph()"
 
