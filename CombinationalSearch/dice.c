18040073ec088089a07c176b287872df29b53ee7
kiranajij <kiranajij216@gmail.com>
2019-04-30 20:45:30 +0530

    dice problem added and merged

--- CombinationalSearch/dice.c
+++ CombinationalSearch/dice.c
@@ -1,36 +1,47 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+
 
 int is_solution(int a[], int k, int n, int sum){
     return (n == k);
 }
-void print_arr(int a[], int start, int end){
-    for (int i=start, i<=end; i++) printf("%d ", arr[i]);
+void print_arr(int arr[], int start, int end){
+    for (int i=start; i<=end; i++) {printf("%d ", arr[i]);}
     printf("\n");
 }
 void process_solution(int a[], int k, int n, int sum){
     if (sum != 0) return;
     print_arr(a, 1, k);
 }
-int generate_candidates(int a[], int k, int n, int sum, int[] cand){
+int generate_candidates(int a[], int k, int n, int sum, int cand[]){
     int n_cand = 0;
     for (int i=1; i<=6; i++){
-        if (i<sum){
+        /*
+        * uncommenting first one will give you all the
+        * possible results.
+        * the second one print one combination only once.
+        * so if "1 1 2" gets printed, "2 1 1" or "1 2 1" will
+        * not get printed
+        */
+        //if(i<=sum)
+        if (i<=sum && i>a[k-1])
+        {
             cand[n_cand] = i;
             n_cand++;
         }
     }
-    return n_cand
+    return n_cand;
 }
 int run(int a[], int k, int n, int sum){
     if (is_solution(a, k, n, sum)){
-        process_solution(a, k, n, sum)
+        process_solution(a, k, n, sum);
     } else {
         k++;
         int cand[6];
         int n_cand = generate_candidates(a, k, n, sum, cand);
         for (int i=0; i<n_cand; i++){
-            a[k] = cand[i]
+            a[k] = cand[i];
             run(a, k, n, sum-a[k]);
         }
     }
